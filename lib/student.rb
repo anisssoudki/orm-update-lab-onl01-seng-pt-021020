a840eaaeac3c26b7d3e1e59131a594dbc3295e1b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-07 20:47:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,17 +34,17 @@ attr_accessor  :name, :grade, :id
   
   
   def save
-  if self.id
-    self.update
-  else
-    sql = <<-SQL
-      INSERT INTO songs (name, grade)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end
-end
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+     end
+    end
   end
   
   def self.create
