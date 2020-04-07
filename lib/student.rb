5bc1522e11c6b93ed36bf23e803696c10ba70e5b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-07 20:41:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   
-  def initialize(id = nil, name, grade)
+  def initialize(name, grade, id =nil)
     @id = id 
     @name = name 
     @grade = grade
